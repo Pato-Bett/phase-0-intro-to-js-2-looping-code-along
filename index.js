4093dd86f55ead8e8f39ccd68001e3c0f8c6783f
Pato-Bett <patobett588@gmail.com>
2023-10-10 21:38:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray,event){
+    let cardsArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cardsArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardsArray
+}
+function countDown(num){
+    while (num>=0) {
+        console.log(num)
+        num = num-1
+    }
+}
